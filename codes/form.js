2beb9fcd6638ca9ad5b35a9239459f8d2ec9d311
gelsiiiella <140424378+gelsiiiella@users.noreply.github.com>
2024-04-25 14:16:14 +0800

    Updated changes

--- codes/form.js
+++ codes/form.js
@@ -8,12 +8,12 @@ function validatePassword() {
     }
     return true;
 }
+
 function resetForm() {
     document.forms["FormName"].reset();
 }
 
 document.getElementById("reset").addEventListener("click", resetForm);
-
 
 document.forms["FormName"].addEventListener("submit", function(event) {
     if (!validatePassword()) {
@@ -25,6 +25,14 @@ function saveAndTransfer() {
     var formData = {
         txtUserName: document.getElementsByName('txtUserName')[0].value,
         txtName: document.getElementsByName('txtName')[0].value,
+        password: document.getElementsByName('password')[0].value,
+        confirm_password: document.getElementsByName('confirm_password')[0].value,
+        SType: document.querySelector('input[name="SType"]:checked').value,
+        Gender: document.querySelector('input[name="Gender"]:checked').value,
+        Year: document.getElementsByName('Year')[0].value,
+        Course: document.getElementsByName('Course')[0].value,
+        Interest: getSelectedInterests(),
+        comment: document.getElementsByName('comment')[0].value
     };
 
     var serializedFormData = JSON.stringify(formData);
@@ -47,15 +55,23 @@ function saveAndTransfer() {
                     <p><strong>Student Type:</strong> ${formData.SType}</p>
                     <p><strong>Gender:</strong> ${formData.Gender}</p>
                     <p><strong>Year:</strong> ${formData.Year}</p>
-                    <p><strong>Year:</strong> ${formData.Year}</p>
                     <p><strong>Course:</strong> ${formData.Course}</p>
                     <p><strong>Interests:</strong> ${formData.Interest}</p>
                     <p><strong>Comment:</strong> ${formData.comment}</p>
-
-                    
                 </div>
             </div>
         </body>
         </html>
     `);
+}
+
+function getSelectedInterests() {
+    var interests = document.getElementsByName('Interest');
+    var selectedInterests = [];
+    for (var i = 0; i < interests.length; i++) {
+        if (interests[i].checked) {
+            selectedInterests.push(interests[i].value);
+        }
+    }
+    return selectedInterests.join(', ');
 }
